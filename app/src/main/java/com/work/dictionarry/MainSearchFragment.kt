01f4edbee39581c03cb1ef41901971ddb6fe4fda
Dana <danamarynova@gmail.com>
2020-04-04 14:10:01 +0300

    Initial commit

---
+++ app/src/main/java/com/work/dictionarry/MainSearchFragment.kt
@@ -0,0 +1,7 @@
+package com.work.dictionarry
+
+import androidx.fragment.app.Fragment
+
+class MainSearchFragment: Fragment(R.layout.fragment_main_search) {
+
+}
---
+++ app/src/main/java/com/work/dictionarry/WordInfoFragment.kt
@@ -0,0 +1,6 @@
+package com.work.dictionarry
+
+import androidx.fragment.app.Fragment
+
+class WordInfoFragment: Fragment(R.layout.fragment_word_info) {
+}
